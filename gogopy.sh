878dca8fdf3b7817e874a214227c0f7b86394631
Andrew <15331990+ahuang11@users.noreply.github.com>
2020-12-16 13:56:34 -0600

    Update gogopy.sh

--- gogopy.sh
+++ gogopy.sh
@@ -30,7 +30,7 @@ printf "Export path complete.\n\n"
 echo Upgrading your Anaconda to the latest version.
 $conda init bash
 $conda update -n base -c defaults conda -y
-$conda install -n base nodejs
+$conda install -n base nodejs -y
 $conda install -n base -c conda-forge jupyterlab nb_conda_kernels -y
 $conda install -n base -c conda-forge dask-labextension -y
 jupyter labextension install dask-labextension
