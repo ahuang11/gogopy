f17a6c79717d5903c3585c6168a134cb249e3f72
Andrew <15331990+ahuang11@users.noreply.github.com>
2021-05-26 13:04:15 -0500

    Create gogopy_user.sh

---
+++ gogopy_user.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/ahuang11/gogopy/master/.bash_profile
+wget https://raw.githubusercontent.com/ahuang11/gogopy/master/.screenrc
+wget https://raw.githubusercontent.com/ahuang11/gogopy/master/.vimrc
+
